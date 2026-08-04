9b93daabc0179c192da805a31c78e38e8fa46edc
rm14-mmdi <rohanmansingh10@gmail.com>
2026-08-04 17:24:28 +0530

    Add extract_terrain function to terrain.py

---
+++ slope_and_elevation_extractor/terrain.py
@@ -0,0 +1,5 @@
+def extract_terrain(input_csv=None, asset_id=None, output_csv="terrain.csv"):
+    print("Package is working!")
+    print(f"Input CSV : {input_csv}")
+    print(f"Asset ID  : {asset_id}")
+    print(f"Output    : {output_csv}")
